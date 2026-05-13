560ce61d37cb2f179dd4fabc779be1355b9ef225
Fernando_Polo <polofernando54@gmail.com>
2026-05-13 10:46:47 -0600

    resta de 2 numeros

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ def main():
     print("Hola De Nuevo")
 if __name__ == "__main__":
     main()
+    resultado = resta()
+    print("Hola De Nuevo" , resta)
+
+
+def resta():
+    num1 = int(input("Escribe un numero: "))
+    num2 = int(input("Escribe un segundo numero: "))
+
+    resultado =num1 - num2
+
+print(f"EL resultado de la suma es:{resta}")
